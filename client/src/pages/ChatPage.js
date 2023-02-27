0a23908bb8c1464903ee29c927d9fb918c1c61ba
abc <duranakyol71@gmail.com>
2023-02-27 11:16:40 +0100

    update2!

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -4,7 +4,7 @@ import Room from "../components/Room";
 import io from "socket.io-client";
 import "../assets/styles/ChatPage.css";
 
-const socket = io.connect("https://falcon-chatapp.netlify.app/server");
+const socket = io.connect("https://falcon-chatapp.netlify.app");
 
 function ChatPage() {
   const [username, setUsername] = useState("");
